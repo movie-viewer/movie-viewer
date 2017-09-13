9dc1d87200b58cb45713dd4b7549600749d73b59
Jordan Leslie <this.codedev@gmail.com>
2017-09-13 16:50:10 -0500

    Successfully add movie object to DOM element (p)

--- src/dom.js
+++ src/dom.js
@@ -1,8 +1,10 @@
 const getMovies = require('./getMovies.js');
+
+let p = document.body.getElementsByTagName("p");
+
+
 function changeH1 () {
     let h1 = document.body.getElementsByTagName("h1");
-    console.log(h1[0]);
-
     h1[0].innerHTML = "Loading";
 }
 
@@ -15,7 +17,7 @@ function movieList () {
         console.log(movies[0]);
         // console.log('Here are all the movies:');
         movies.forEach(({title, rating, id}) => {
-            console.log(`id#${id} - ${title} - rating: ${rating}`);
+            p[0].innerHTML =(`id#${id} - ${title} - rating: ${rating}`);
         });
     }).catch((error) => {
         alert('Oh no! Something went wrong.\nCheck the console for details.');
