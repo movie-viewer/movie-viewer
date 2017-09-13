bb80045f9864e305d771f451d87c3d30bb7cf7b0
alxmpotter <alxmpotter@gmail.com>
2017-09-13 16:41:37 -0500

    Movie SPA
    -made movieList function

--- src/dom.js
+++ src/dom.js
@@ -1,4 +1,4 @@
-
+const getMovies = require('./getMovies.js');
 function changeH1 () {
     let h1 = document.body.getElementsByTagName("h1");
     console.log(h1[0]);
@@ -7,5 +7,21 @@ function changeH1 () {
 }
 
 
-
 module.exports = changeH1;
+
+
+function movieList () {
+    getMovies().then((movies) => {
+        console.log(movies[0]);
+        // console.log('Here are all the movies:');
+        movies.forEach(({title, rating, id}) => {
+            console.log(`id#${id} - ${title} - rating: ${rating}`);
+        });
+    }).catch((error) => {
+        alert('Oh no! Something went wrong.\nCheck the console for details.');
+        console.log(error);
+    });
+
+}
+//
+module.exports = movieList;
--- src/index.js
+++ src/index.js
@@ -5,23 +5,15 @@
 import sayHello from './hello';
 sayHello('World');
 const changeH1 = require('./dom');
+const movieList = require("./dom");
+movieList();
+
 // import changeH1 from './dom';
-changeH1();
+
 /**
  * require style imports
  */
 const getMovies = require('./getMovies.js');
-
-getMovies().then((movies) => {
-
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
-  });
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.');
-  console.log(error);
-});
-
+// changeH1()
 
 
