e8067a1d7264f5c57da44ca8fc7db8703614def4
Jordan Leslie <this.codedev@gmail.com>
2017-09-14 10:36:56 -0500

    Add fetchTest listener on submit button

--- src/getMovies.js
+++ src/getMovies.js
@@ -4,3 +4,4 @@ const getMovies = () => {
 };
 
 module.exports = getMovies;
+
--- src/index.js
+++ src/index.js
@@ -7,8 +7,38 @@ sayHello('World');
 const changeH1 = require('./dom');
 const dom = require("./dom");
 
+
+const submitBtn = document.getElementById("submitBtn");
+
+submitBtn.addEventListener('click',
+    () => fetchTest()
+    );
+
+const fetchTest = () => {
+
+    let url = 'api/movies';
+    let options = {
+        headers: {
+            "content-type": "application/json"
+        },
+        method: "POST",
+        body: JSON.stringify(
+            {
+                "title": ,
+                "rating":
+            })
+    };
+    return fetch(url, options)
+        .then(response => response.json())
+        .then(response => console.log("It works!"))
+        .catch(response => console.log("Something went wrong with the fetch"))
+};
+
+
 dom.movieList();
 dom.changeH1();
+
+
 
 // import changeH1 from './dom';
 
