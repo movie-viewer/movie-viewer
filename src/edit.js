78c60f0b4238470f0079990e0c9b31387b231035
Jordan Leslie <this.codedev@gmail.com>
2017-09-15 14:11:50 -0500

    Radio buttons on edit modal are now preselected with current rating

--- src/edit.js
+++ src/edit.js
@@ -1,10 +1,14 @@
 // const getMovies = require('./getMovies.js');
 
 function editMovie () {
+    // let hack = `"#edit-${data-rating}"`;
     $(".editBtn").on("click", function(){
         $("#id").val($(this).attr("data-id"));
         $("#edit-movie-title").val($(this).attr("data-title"));
-        $("#edit-${data-").prop('checked',true);
+        let value = $(this).attr('data-rating');
+
+        $(`input[name=rating][value=${value}]`).prop("checked", true);
+        // $(hack).prop('checked',true);
 
     });
 }
