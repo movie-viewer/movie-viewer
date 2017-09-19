5989f96f36457eee7e473d2968c963ae375e37ca
Jordan Leslie <this.codedev@gmail.com>
2017-09-18 19:10:47 -0500

    Added on document click handlers for edit functions. submitMovieEdit now refreshes the table

--- src/index.js
+++ src/index.js
@@ -2,14 +2,26 @@ const changeH1 = require('./dom');
 const dom = require("./dom");
 const fetchInput = require("./submit");
 const getMovies = require('./getMovies');
-const editMovie = require('./edit');
+const {editMovie, submitMovieEdit} = require('./edit');
+
+
+$(document).ready(function(){
+    $('.modal').modal();
+
+});
+
 
 dom.movieList();
 
 
+//define a class called movie function
+//return promises not functions
 
 
+//A Jquery quirk: now I can assign the listener !on click! rather than on page load
+$(document).on('click', '.submitEdit', ()=> submitMovieEdit().then(()=> dom.movieList()));
 
+$(document).on('click', '.generalEdit', editMovie);
 
 
 
