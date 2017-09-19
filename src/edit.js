813ccc0bf2b6090f98c359cff8941e5cde6107d3
Jordan Leslie <this.codedev@gmail.com>
2017-09-18 19:09:03 -0500

    removed click function from editMovie and submitMovie. Made movieSettings a global variable. Cleaned code and removed console logs

--- src/edit.js
+++ src/edit.js
@@ -1,20 +1,48 @@
-// const getMovies = require('./getMovies.js');
+// Global access of movieSettings variable across both functions
+let movieSettings ={};
 
-function editMovie () {
-    // let hack = `"#edit-${data-rating}"`;
-    $(".editBtn").on("click", function(){
-        $("#id").val($(this).attr("data-id"));
-        $("#edit-movie-title").val($(this).attr("data-title"));
-        let value = $(this).attr('data-rating');
+function editMovie() {
+        //Data Attr dynamically generated on general button anchor tag (dom.js)
+       movieSettings = {
+            movieId: parseInt($(this).attr("data-id")),
+            movieRating: $(this).attr('data-rating'),
+            movieTitle: $(this).attr("data-title")
+        };
 
-        $(`input[name=rating][value=${value}]`).prop("checked", true);
-        // $(hack).prop('checked',true);
+        //Pre-selects rating inside edit modal
+        $(`input[name=rating][value="${movieSettings.movieRating}"]`).prop("checked", true);
 
-    });
+        //Movie title to be edited appears inside input field
+        $("#edit-movie-title").val(movieSettings.movieTitle);
 }
 
-$(document).ready(function(){
-    $('.modal').modal();
-});
+const submitMovieEdit = () => {
 
-module.exports = editMovie;
+        //Update global object with user's edits
+        movieSettings.movieRating = $('input[name=rating]:checked').val();
+        movieSettings.movieTitle = $("#edit-movie-title").val();
+
+
+        //Return a promise via fetch
+        let url = `api/movies/${movieSettings.movieId}`;
+        let options = {
+            headers: {
+                "content-type": "application/json"
+            },
+            method: "PUT",
+            body: JSON.stringify(
+                {
+                    "title": movieSettings.movieTitle,
+                    "rating": movieSettings.movieRating,
+                    "id": movieSettings.movieId
+                })
+        };
+            return fetch(url, options)
+            .then(response => response.json());
+
+};
+
+
+
+
+module.exports = {submitMovieEdit, editMovie};
