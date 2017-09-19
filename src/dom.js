0b64990921461859bbd856bd04d5a07ef50136ea
Jordan Leslie <this.codedev@gmail.com>
2017-09-18 19:06:24 -0500

    Wrapped ID in its own table cell

--- src/dom.js
+++ src/dom.js
@@ -1,34 +1,37 @@
 const getMovies = require('./getMovies.js');
-const editMovie = require('./edit');
+const edit = require('./edit');
 
 function movieList () {
     //Erase the loading message after movie list loads
-    let editBtn = ``;
     let tableBody = document.querySelector("#tableBody");
 
-    tableBody.append = "Loading";
+    tableBody.innerText = "Loading...";
 
     getMovies().then((movies) => {
         tableBody.innerText = "";
 
         movies.forEach(({title, rating, id}) => {
-
             tableBody.innerHTML +=
-                `<td>${id} - ${title}</td>
+                `<td>${id}</td>
+                <td>${title}</td>
                 <td>${rating}</td>
                 <td>
-                <a data-target="modal1" class="waves-effect waves-light btn editBtn btn modal-trigger" data-id="${id}" data-title="${title}" data-rating="${rating}"><i class="material-icons left">create</i>Edit</a>
+                <a data-target="modal1"
+                class="generalEdit waves-effect waves-light btn btn modal-trigger"
+                data-id="${id}"
+                data-title="${title}"
+                data-rating="${rating}">
+                <i class="material-icons left">create</i>Edit</a>
+
+                <a class="waves-effect waves-light btn editBtn btn modal-trigger red darken-3">
+                <i class="material-icons left">clear</i>Delete</a>
                 </td>`
         });
-        editMovie();
     }).catch((error) => {
         alert('Oh no! Something went wrong.\nCheck the console for details.');
         console.log(error);
     });
 }
 
-// function editMovie() {
-//     let
-// }
 
 module.exports = {movieList};
